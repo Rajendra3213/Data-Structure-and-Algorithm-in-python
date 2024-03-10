555386f1cd366c38710a9d863e4799660dc67cd7
rajendraacharya1ls <rajendraacharya1ls@gmail.com>
2024-03-10 23:45:42 +0545

    insertion sort

---
+++ sorting algorithms/insertion_sort.py
@@ -0,0 +1,20 @@
+def insertion_sort(lst):
+    
+    for i in range(1, len(lst)):
+        key = lst[i]
+        
+        j = i - 1
+        
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
+            j = j - 1
+
+        lst[j + 1] = key
+    
+    return lst
+
+data =  [7, 4, 1, 3, 2]
+
+print(f"Unsorted List: {data}")
+sorted_list = insertion_sort(data)
+print(f"Sorted List: {sorted_list}")
