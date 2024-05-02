fcf52d608a499a6a687251c6d0a5f30e707b3828
rajendraacharya1ls <rajendraacharya1ls@gmail.com>
2024-05-03 01:23:32 +0545

    tree traversing added

--- Tree/traverse_based_on_level.py
+++ Tree/traverse_based_on_level.py
@@ -1,68 +1,45 @@
-# create a class to handle the creation of node and addition of child nodes
+# Replace ___ with your code
+
+# a class to create and add nodes
 class TreeNode:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.children = []
-
-    def add_child(self, child):
-        self.children.append(child)
-
-# class to handle the creation of trees and operations performed in it
+    def add_child(self,data):
+        self.children.append(data)
+    
+# a class to create a tree and perform tree operations
 class Tree:
-    def __init__(self, root=None):
+    def __init__(self,root=None):
         self.root = root
     
-    def get_nodes_at_k(self,node, k):
-        if node is None:
-            return []
-        elif k == 0:
-            return [node.data]
-        else:
-            result = []
+    def get_nodes_at_depth(self,node,depth):
+        if not node:
+            return 
+        if depth == 0:
+            return node.data
+        if node and depth >0:
+            result = 0
             for child in node.children:
-                result += self.get_nodes_at_k(child, k - 1)
+                result+=self.get_nodes_at_depth(child,depth-1)
             return result
 
-# creating our root node
-root = TreeNode("Book")
+# create the root node
+root = TreeNode('root')
 
-# creating the children of root
+# create leaf nodes
 child1 = TreeNode(1)
-child2 = TreeNode(2)
-child3 = TreeNode(3)
+child1_1 = TreeNode(2)
+child1_1_1 = TreeNode(3)
 
-# creating our leaf nodes
-child1_1 = TreeNode(1.1)
-child1_2 = TreeNode(1.2)
-child1_3 = TreeNode(1.3)
-child1_4 = TreeNode(1.4)
-child2_1 = TreeNode(2.1)
-child3_1 = TreeNode(3.1)
-child3_2 = TreeNode(3.2)
-child3_3 = TreeNode(3.3)
-
-# describing relationships between the nodes
-
-# add children to the root node
+# describe relationships between the nodes
 root.add_child(child1)
-root.add_child(child2)
-root.add_child(child3)
-
-# add children to child1
 child1.add_child(child1_1)
-child1.add_child(child1_2)
-child1.add_child(child1_3)
-child1.add_child(child1_4)
+child1_1.add_child(child1_1_1)
 
-# add children to child2
-child2.add_child(child2_1)
-
-# add children to child3
-child3.add_child(child3_1)
-child3.add_child(child3_2)
-child3.add_child(child3_3)
-
-# creating our tree
+# create the tree
 tree = Tree(root)
-for i in range(0,3):
-    print(f"nodes at depth {i}: {tree.get_nodes_at_k(root, i)}")
+
+# get nodes at each level
+for i in range(0,4):
+    print(f"Nodes at Level {i}: {tree.get_nodes_at_depth(root, i)}")
